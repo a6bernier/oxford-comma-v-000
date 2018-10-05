c46511915802cc0c3ea282970438718d45da2af0
Andrew Bernier <a6bernier@gmail.com>
2018-10-05 18:30:04 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,6 +4,6 @@ def oxford_comma(array)
   elsif array.length == 2
     array.join(" and ")
   elsif array.length == 3
-    array.insert(-1, "and")
+    array.insert(-1, "and").join(", ")
   end
 end
