63b04bf697392cac91b1b1402c34a887823ba93b
Andrew Bernier <a6bernier@gmail.com>
2018-10-05 18:55:41 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -6,7 +6,6 @@ def oxford_comma(array)
   elsif array.length == 3
     new_array = []
     new_string = ""
-    new_array << array.insert(-2, "and")
-    new_array.insert(", ")
+    array.insert(-2, "and")
   end
 end
